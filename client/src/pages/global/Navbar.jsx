fa9c47ed92f9bea9626a105a1d4e0e69e295da1c
Sheelove Dol <siesel26@gmail.com>
2023-04-30 00:26:56 -0700

    closing shop for the night

--- client/src/pages/global/Navbar.jsx
+++ client/src/pages/global/Navbar.jsx
@@ -1,9 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { MdPersonOutline, MdOutlineShoppingBag, MdOutlineSearch, MdOutlineMenu } from "react-icons/md";
-import { SlUser } from "react-icons/sl"
+import { setIsCartOpen } from "../../state"
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  // Accessing cartSlice 
+  const cart = useSelector((state) => state.cart.cart);
+
 
   return (
     <nav className="flex items-center justify-center h-[60px] w-full fixed top-0 left-0 z-1 bg-white/95 border border-red-300">
@@ -13,10 +19,21 @@ const Navbar = () => {
           Fashionly
         </div>
         <div className="border flex justify-between gap-5 z-2">
-          <MdOutlineSearch className="hover cursor-pointer"/>
-          <MdPersonOutline className="hover cursor-pointer"/>
-          <MdOutlineShoppingBag className="hover cursor-pointer"/>
-          <MdOutlineMenu className="hover cursor-pointer"/>
+          <button>
+            <MdOutlineSearch className="hover cursor-pointer"/>
+          </button>
+          <button>
+            <MdPersonOutline className="hover cursor-pointer"/>
+          </button>
+          <button onClick={() => dispatch(setIsCartOpen({}))}
+            className="border relative"  
+          >
+            <div className="badge badge-xs absolute border top-0 bg-red-500 rounded-full text-white border-none " >{cart.length}</div>
+            <MdOutlineShoppingBag className="hover cursor-pointer"/>
+          </button>
+          <button>
+            <MdOutlineMenu className="hover cursor-pointer"/>
+          </button>
         </div>
       </div>
     </nav>
