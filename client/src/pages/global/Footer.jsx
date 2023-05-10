c7ecffeae9451f1d3aa378c00e27dd070443172c
Sheelove Dol <siesel26@gmail.com>
2023-05-10 11:51:51 -0700

    Fixed fontSize with FooterText component

--- client/src/pages/global/Footer.jsx
+++ client/src/pages/global/Footer.jsx
@@ -2,36 +2,41 @@ import React from 'react'
 
 const Footer = () => {
 
-    //TODO: Make it easier to edit by creating components for pTags
+  // Component to make text styling more dynamic
+  const FooterText = ({ children }) => {
+    return <p className="md:mb-7 text-xs">{children}</p>;
+  };
+  
+
   return (
     <div className='py-10 px-0 mt-16 bg-neutral-200 '>
       <div className='md:flex w-4/5 m-auto justify-between md:gap-y-7  md:gap-x-7  '
       >
         <div className='md:w-1/5 md:w-3/10 lg:w-2/5 '>
             <h4 className='mb-3 sm:mb-7 font-bold text-secondary-content'>Fashionly</h4>
-            <div>
+            <FooterText>
                 Uspendisse vel mi ac augue commodo ultricies a sed risus. Vestibulum eu elit pulvinar, convallis ligula id, lobortis nibh. Praesent varius id nulla a hendrerit. 
-            </div>
+            </FooterText>
         </div>
         <div className='mt-7 md:mt-0'>
             <h4 className='font-bold mb-3 md:mb-7'>About Us</h4>
-            <p className='md:mb-7'>Our Stores</p>
-            <p className='md:mb-7'>Careers</p>
-            <p className='md:mb-7'>Terms & Conditions</p>
-            <p className='md:mb-7'>Privacy Policy</p>
+            <FooterText >Our Stores</FooterText>
+            <FooterText >Careers</FooterText>
+            <FooterText >Terms & Conditions</FooterText>
+            <FooterText>Privacy Policy</FooterText>
         </div>
         <div className='mt-7 md:mt-0 '>
             <h4 className='font-bold mb-3 md:mb-7'>Customer Care</h4>
-            <p className='md:mb-7'>Track Your Orders</p>
-            <p className='md:mb-7'>Return & refunds</p>
-            <p className='md:mb-7'>Help Center</p>
-            <p className='md:mb-7'>Corporate & Bulk Purchasing</p>
+            <FooterText>Track Your Orders</FooterText>
+            <FooterText>Return & refunds</FooterText>
+            <FooterText>Help Center</FooterText>
+            <FooterText>Corporate & Bulk Purchasing</FooterText>
         </div>
         <div className='md:w-1/5 :w-3/10 lg:w-2/5 mt-7 md:mt-0'>
             <h4 className='font-bold mb-3 md:mb-7'>Contact us</h4>
-            <p className='md:mb-7'>1922 Clark Avenue, Toronto, ON T3T9V8</p>
-            <p className='md:mb-7'>Email: fashionlyrandom@gmail.com</p>
-            <p className='md:mb-7'>Phone: (999) 888-7766</p>
+            <FooterText>1922 Clark Avenue, Toronto, ON T3T9V8</FooterText>
+            <FooterText>Email: fashionlyrandom@gmail.com</FooterText>
+            <FooterText>Phone: (999) 888-7766</FooterText>
         </div>
       </div>
     </div>
